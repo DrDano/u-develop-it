c4e2f78cd989ed68c1467970e79a4c889968b079
DrDano <danielharned@gmail.com>
2022-01-24 23:50:55 -0800

    adding config file and connection to database in server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2');
+const {user, pw} = require('./project.config');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // express middleware
-app.use(express.urlencoded({ extended: false }));app.use(express.json());
+app.use(express.urlencoded({ extended: false })); app.use(express.json());
+
+// connect to the SQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: user,
+        password: pw,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
