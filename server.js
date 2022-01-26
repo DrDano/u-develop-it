bf79590f6a4cbfc9b84b45d4dfc86c428823a554
DrDano <danielharned@gmail.com>
2022-01-25 17:14:46 -0800

    adding joins to both get routes in server.js

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -42,7 +46,12 @@ app.get('/api/candidates', (req, res) => {
 
 app.get('/api/candidate/:id', (req, res) => {
     const query = [req.params.id]
-    const sql = `SELECT * FROM candidates WHERE id = ?;`
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id
+                 WHERE candidates.id = ?`;
 
     db.query(sql, query, (err, row) => {
         if (err) {
