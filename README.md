098d215dbe0f1b8318a77a7ac3fb21a6ac35ef5d
DrDano <danielharned@gmail.com>
2022-02-05 22:36:43 -0800

    refactoring and reorganizing code to match module

--- README.md
+++
@@ -1,2 +0,0 @@
-# u-develop-it
-A voting application with data storage and access built in.
--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-const apiRoutes = require('./routes/apiRoutes');
-const express = require("express");
-const inputCheck = require("./utils/inputCheck");
-const db = require("./db/connection")
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// express middleware
-app.use(express.urlencoded({ extended: false }));
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use('/api', apiRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// default response for any other request
-app.use((req, res) => {
-  res.status(404).end();
-});
+app.use(require('./controllers/'));
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
